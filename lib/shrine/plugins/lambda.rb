4c06fbcea666cc090e3a236075033039c8aca8fb
texpert <branzeanu.aurel@gmail.com>
2017-12-27 01:36:59 +0200

    Fixed Lambda plugin configuration method to iterate through all the settings.
    Implemented class and instance `:lambda` and `:lambda_function_list` methods for client initialization-invocation and function list retrieval. Function list is memoized into the `Shrine.opts` hash.
    
    `Shrine.lambda_function_list` could be put into Shrine initializer to load the list on application start.

--- lib/shrine/plugins/lambda.rb
+++ lib/shrine/plugins/lambda.rb
@@ -5,19 +5,18 @@ require 'aws-sdk-lambda'
 class Shrine
   module Plugins
     module Lambda
-      # Required AWS credentials:
-      SETTINGS = { access_key_id: :required, region: :required, callback_url: :optional,
-                   secret_access_key: :required }
+      SETTINGS = { access_key_id: :required, callback_url: :optional, region: :required,
+                   secret_access_key: :required }.freeze
 
       Error = Class.new(Shrine::Error)
 
       # If promoting was not yet overridden, it is set to automatically trigger
       # Lambda processing defined in `Shrine#lambda_process`.
       def self.configure(uploader, settings = {})
-        settings.each do |option|
-          uploader.opts[option] = settings.fetch(option, uploader.opts[option])
-          if SETTINGS[option] == :required && uploader.opts[option].nil?
-            raise Error, "The :#{option} is required for Lambda plugin"
+        SETTINGS.each_key do |key, value|
+          uploader.opts[key] = settings.fetch(key, uploader.opts[key])
+          if value == :required && uploader.opts[key].nil?
+            raise Error, "The :#{key} is required for Lambda plugin"
           end
         end
 
@@ -28,6 +27,36 @@ class Shrine
       # It loads the backgrounding plugin, so that it can override promoting.
       def self.load_dependencies(uploader, _opts = {})
         uploader.plugin :backgrounding
+      end
+
+      module ClassMethods
+        # Creates a new AWS Lambda client
+        def lambda
+          Aws::Lambda::Client.new(access_key_id:     opts[:access_key_id],
+                                  secret_access_key: opts[:secret_access_key],
+                                  region:            opts[:region])
+        end
+
+        def lambda_function_list()
+          opts[:lambda_function_list] = lambda.list_functions(# master_region: s3_options[:region],
+                                                              function_version: 'ALL', # accepts ALL
+                                                              # marker: 'String',
+                                                              max_items: 100)
+        end
+      end
+
+      module InstanceMethods
+        # A cached instance of an AWS Lambda client.
+        def lambda
+          @lambda ||= self.class.lambda
+        end
+
+        def lambda_function_list(force: false)
+          fl = self.opts[:lambda_function_list]
+          return fl unless force || fl.nil? || fl.empty?
+          self.class.lambda_function_list
+          self.opts[:lambda_function_list]
+        end
       end
     end
 
